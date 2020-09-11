807603190830659025f98f0cd4e20ca19fba4bb8
Ben Peddell <klightspeed@killerwolves.net>
2020-09-11 21:01:15 +1000

    Initial commit

---
+++ RegionMap-datagen.py
@@ -0,0 +1,82 @@
+#!/bin/env python3
+
+from PIL import Image
+from numpy import asarray
+regionmap = asarray(Image.open('RegionMap.png'))
+region1 = 42 * 4
+
+regions = [
+    None,
+    "Galactic Centre",
+    "Empyrean Straits",
+    "Ryker's Hope",
+    "Odin's Hold",
+    "Norma Arm",
+    "Arcadian Stream",
+    "Izanami",
+    "Inner Orion-Perseus Conflux",
+    "Inner Scutum-Centaurus Arm",
+    "Norma Expanse",
+    "Trojan Belt",
+    "The Veils",
+    "Newton's Vault",
+    "The Conduit",
+    "Outer Orion-Perseus Conflux",
+    "Orion-Cygnus Arm",
+    "Temple",
+    "Inner Orion Spur",
+    "Hawking's Gap",
+    "Dryman's Point",
+    "Sagittarius-Carina Arm",
+    "Mare Somnia",
+    "Acheron",
+    "Formorian Frontier",
+    "Hieronymus Delta",
+    "Outer Scutum-Centaurus Arm",
+    "Outer Arm",
+    "Aquila's Halo",
+    "Errant Marches",
+    "Perseus Arm",
+    "Formidine Rift",
+    "Vulcan Gate",
+    "Elysian Shore",
+    "Sanguineous Rim",
+    "Outer Orion Spur",
+    "Achilles's Altar",
+    "Xibalba",
+    "Lyra's Song",
+    "Tenebrae",
+    "The Abyss",
+    "Kepler's Crest",
+    "The Void"
+]
+
+lines = []
+for l in regionmap:
+    rle = []
+    p = 0
+    n = 0
+    for px in l:
+        px = 0 if px == 0 else (region1 - px) // 4 + 1
+        if px != p:
+            rle.append((n, p))
+            p = px
+            n = 0
+        else:
+            n += 1
+    rle.append((n, p))
+    lines.append(rle)
+with open('RegionMapData.py', 'wt') as f:
+    f.write('#!/bin/env python3\n')
+    f.write('\n')
+    f.write('regions = [\n')
+    for r in regions:
+        f.write('    {0},\n'.format(repr(r)))
+    f.write(']\n')
+    f.write('\n')
+    f.write('regionmap = [\n')
+    for l in lines:
+        f.write('    {0},\n'.format(repr(l)))
+    f.write(']\n')
+    f.write('\n')
+
---
+++ RegionMap.py
@@ -0,0 +1,88 @@
+#!/bin/env python3
+
+import urllib.parse
+import urllib.request
+import sys
+import json
+from RegionMapData import regions, regionmap
+
+pixelsize = 4096 / 83
+x0 = -49985
+y0 = -40985
+z0 = -24105
+
+def findRegion(x, y, z):
+    px = int((x - x0) / pixelsize)
+    pz = int(2048 - (z - z0) / pixelsize)
+    
+    if pz < 0 or pz > len(regionmap):
+        return None
+    else:
+        row = regionmap[pz]
+        rx = 0
+        
+        for rl, pv in row:
+            if px < rx + rl:
+                break
+            else:
+                rx += rl
+
+        if pv == 0:
+            return None
+        else:
+            return (pv, regions[pv])
+
+def main():
+    sysname = sys.argv[1]
+    url = 'https://www.edsm.net/api-v1/systems?systemName=' + urllib.parse.quote(sysname) + '&coords=1&showId=1'
+    with urllib.request.urlopen(url) as f:
+        systems = json.load(f)
+    for system in systems:
+        region = findRegion(system['coords']['x'], system['coords']['y'], system['coords']['z'])
+        if region is not None:
+            print('System {0} at ({1},{2},{3}) is in region {4} ({5})'.format(
+                system['name'],
+                system['coords']['x'],
+                system['coords']['y'],
+                system['coords']['z'],
+                region[0],
+                region[1]
+            ))
+        else:
+            print('System {0} at ({1},{2},{3}) is outside the region map'.format(
+                system['name'],
+                system['coords']['x'],
+                system['coords']['y'],
+                system['coords']['z']
+            ))
+
+        id64 = system['id64']
+        masscode = id64 & 7
+        z = (((id64 >> 3) & (0x3FFF >> masscode)) << masscode) * 10 + z0
+        y = (((id64 >> (17 - masscode)) & (0x1FFF >> masscode)) << masscode) * 10 + y0
+        x = (((id64 >> (30 - masscode * 2)) & (0x3FFF >> masscode)) << masscode) * 10 + x0
+        region2 = findRegion(x, y, z)
+        if region2 != region:
+            print('Boxel of system {0} is at ({1},{2},{3})'.format(
+                system['name'],
+                x,
+                y,
+                z
+            ))
+            if region2 is not None:
+                print('({0},{1},{2}) is in region {3} ({4})'.format(
+                    x,
+                    y,
+                    z,
+                    region2[0],
+                    region2[1]
+                ))
+            else:
+                print('({0},{1},{2}) is outside the region map'.format(
+                    x,
+                    y,
+                    z
+                ))
+
+if __name__ == '__main__':
+    main()
