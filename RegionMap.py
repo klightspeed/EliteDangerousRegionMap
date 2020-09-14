0c876e300224c7a8e9cdb440abe32340f780bd6b
Ben Peddell <klightspeed@killerwolves.net>
2020-09-14 16:55:50 +1000

    Add usage; accept multiple systems

--- RegionMap.py
+++ RegionMap.py
@@ -34,56 +34,60 @@ def findRegion(x, y, z):
             return (pv, regions[pv])
 
 def main():
-    sysname = sys.argv[1]
-    url = 'https://www.edsm.net/api-v1/systems?systemName=' + urllib.parse.quote(sysname) + '&coords=1&showId=1'
-    with urllib.request.urlopen(url) as f:
-        systems = json.load(f)
-    for system in systems:
-        region = findRegion(system['coords']['x'], system['coords']['y'], system['coords']['z'])
-        if region is not None:
-            print('System {0} at ({1},{2},{3}) is in region {4} ({5})'.format(
-                system['name'],
-                system['coords']['x'],
-                system['coords']['y'],
-                system['coords']['z'],
-                region[0],
-                region[1]
-            ))
-        else:
-            print('System {0} at ({1},{2},{3}) is outside the region map'.format(
-                system['name'],
-                system['coords']['x'],
-                system['coords']['y'],
-                system['coords']['z']
-            ))
+    if len(sys.argv) <= 1:
+        print('Usage: {0} "System Name" [...]'.format(sys.argv[0]))
+        return
 
-        id64 = system['id64']
-        masscode = id64 & 7
-        z = (((id64 >> 3) & (0x3FFF >> masscode)) << masscode) * 10 + z0
-        y = (((id64 >> (17 - masscode)) & (0x1FFF >> masscode)) << masscode) * 10 + y0
-        x = (((id64 >> (30 - masscode * 2)) & (0x3FFF >> masscode)) << masscode) * 10 + x0
-        region2 = findRegion(x, y, z)
-        if region2 != region:
-            print('Boxel of system {0} is at ({1},{2},{3})'.format(
-                system['name'],
-                x,
-                y,
-                z
-            ))
-            if region2 is not None:
-                print('({0},{1},{2}) is in region {3} ({4})'.format(
-                    x,
-                    y,
-                    z,
-                    region2[0],
-                    region2[1]
+    for sysname in sys.argv[1:]:
+        url = 'https://www.edsm.net/api-v1/systems?systemName=' + urllib.parse.quote(sysname) + '&coords=1&showId=1'
+        with urllib.request.urlopen(url) as f:
+            systems = json.load(f)
+        for system in systems:
+            region = findRegion(system['coords']['x'], system['coords']['y'], system['coords']['z'])
+            if region is not None:
+                print('System {0} at ({1},{2},{3}) is in region {4} ({5})'.format(
+                    system['name'],
+                    system['coords']['x'],
+                    system['coords']['y'],
+                    system['coords']['z'],
+                    region[0],
+                    region[1]
                 ))
             else:
-                print('({0},{1},{2}) is outside the region map'.format(
+                print('System {0} at ({1},{2},{3}) is outside the region map'.format(
+                    system['name'],
+                    system['coords']['x'],
+                    system['coords']['y'],
+                    system['coords']['z']
+                ))
+
+            id64 = system['id64']
+            masscode = id64 & 7
+            z = (((id64 >> 3) & (0x3FFF >> masscode)) << masscode) * 10 + z0
+            y = (((id64 >> (17 - masscode)) & (0x1FFF >> masscode)) << masscode) * 10 + y0
+            x = (((id64 >> (30 - masscode * 2)) & (0x3FFF >> masscode)) << masscode) * 10 + x0
+            region2 = findRegion(x, y, z)
+            if region2 != region:
+                print('Boxel of system {0} is at ({1},{2},{3})'.format(
+                    system['name'],
                     x,
                     y,
                     z
                 ))
+                if region2 is not None:
+                    print('({0},{1},{2}) is in region {3} ({4})'.format(
+                        x,
+                        y,
+                        z,
+                        region2[0],
+                        region2[1]
+                    ))
+                else:
+                    print('({0},{1},{2}) is outside the region map'.format(
+                        x,
+                        y,
+                        z
+                    ))
 
 if __name__ == '__main__':
     main()
